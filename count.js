046709997f41d690478647d403c8e9f811d38388
ahmedfarook <ahmedgaziyani@gmail.com>
2021-07-09 19:19:47 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,13 @@
+
+// //console.log(document.getElementById("").textContent)
+
+// document.getElementById("upload").addEventListener("click", function () {
+
+    
+//     console.log(document.getElementById("amount").value);
+
+// }
+
+// )
+
+// console.log(document.getElementById("amount").value)
